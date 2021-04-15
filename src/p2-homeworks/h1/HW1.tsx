9e8a2c369e93c3d52735a736bd7cb397c3850140
makcsholonik <makcsholonik@gmail.com>
2021-04-15 20:55:59 +0300

    HW1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import {Message} from "./Message";
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    name: 'Maxim',
+    message: 'npm start нажимал?',
+    time: '22:00'
 }
 
 function HW1() {
@@ -12,16 +13,13 @@ function HW1() {
         <div>
             <hr/>
             homeworks 1
-
-            {/*should work (должно работать)*/}
-
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
-
+            should work (должно работать)
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
             <hr/>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeMessage/>*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
+import cl from './Message.module.css'
 
-function Message() {
-    return (
-        <div>
-
-        </div>
-    )
+type MessagePropsType = {
+	avatar : string
+	name : string
+	message : string
+	time : string
 }
 
-export default Message
+export function Message ( props : MessagePropsType ) {
+	return (
+		<div className={ cl.message }>
+			<img className={cl.image} src={ props.avatar } alt="avatar"/>
+			<div className={ cl.content }>
+				<div className={ cl.name }>{ props.name }</div>
+				<div className={ cl.message }>{ props.message }</div>
+				<div className={ cl.time }>{ props.time }</div>
+			</div>
+			<div className={cl.clear}></div>
+		</div>
+	)
+}
