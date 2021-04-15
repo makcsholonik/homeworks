50ba1259f1fea70ce871b4c2e4fe8d4d33d97eb4
makcsholonik <makcsholonik@gmail.com>
2021-04-15 22:46:37 +0300

    HW2

--- src/p2-homeworks/h2/Affairs.tsx
+++ src/p2-homeworks/h2/Affairs.tsx
@@ -1,38 +1,40 @@
 import React from 'react'
 import Affair from './Affair'
-import {AffairType} from './HW2'
+import { AffairType, FilterType } from './HW2'
+import st from './Affairs.module.css'
 
 type AffairsPropsType = { // need to fix any
-    data: any
-    setFilter: any
-    deleteAffairCallback: any
+	data : Array<AffairType>
+	setFilter : ( filter : FilterType ) => void
+	deleteAffairCallback : ( _id : number ) => void
+   filter: FilterType
 }
 
-function Affairs(props: AffairsPropsType) {
-    const mappedAffairs = props.data.map((a: AffairType) => (
-        <Affair // should work
-            key={a._id} // кеи ОБЯЗАТЕЛЬНЫ в 99% - так что лучше их писать всегда при создании компонент в мапе
-            affair={a}
-            deleteAffairCallback={props.deleteAffairCallback}
-        />
-    ))
+function Affairs ( props : AffairsPropsType ) {
+	const mappedAffairs = props.data.map ( ( a : AffairType ) => (
+		<Affair
+			key={ a._id }
+			affair={ a }
+			deleteAffairCallback={ props.deleteAffairCallback }
+		/>
+	) )
 
-    const setAll = () => {} // need to fix
-    const setHigh = () => {}
-    const setMiddle = () => {}
-    const setLow = () => {}
+	const setAll = () => {props.setFilter ( 'all' )}
+	const setHigh = () => {props.setFilter ( 'high' )}
+	const setMiddle = () => {props.setFilter ( 'middle' )}
+	const setLow = () => {props.setFilter ( 'low' )}
 
-    return (
-        <div>
+   const setClass = (className: FilterType) => props.filter === className ? st.active : ''
 
-            {mappedAffairs}
-
-            <button onClick={setAll}>All</button>
-            <button onClick={setHigh}>High</button>
-            <button onClick={setMiddle}>Middle</button>
-            <button onClick={setLow}>Low</button>
-        </div>
-    )
+   return (
+      <div>
+         {mappedAffairs}
+         <button onClick={setAll} className={setClass('all')}>All</button>
+         <button onClick={setHigh} className={setClass('high')}>High</button>
+         <button onClick={setMiddle} className={setClass('middle')}>Middle</button>
+         <button onClick={setLow} className={setClass('low')}>Low</button>
+      </div>
+   );
 }
 
 export default Affairs
