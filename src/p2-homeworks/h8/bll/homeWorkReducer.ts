190a7896d93b44b2f698a52c8719a224eeed9ed8
makcsholonik <makcsholonik@gmail.com>
2021-05-26 23:09:42 +0300

    HW8

--- src/p2-homeworks/h8/bll/homeWorkReducer.ts
+++ src/p2-homeworks/h8/bll/homeWorkReducer.ts
@@ -1,13 +1,28 @@
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': {
-            // need to fix
-            return state
-        }
-        case 'check': {
-            // need to fix
-            return state
-        }
-        default: return state
-    }
+import { UserType } from "../HW8";
+
+type ActionType = { type : 'sort', payload : 'up' | 'down' } | { type : 'check', payload : 18 }
+
+export const homeWorkReducer = ( state : Array<UserType>, action : ActionType ) : Array<UserType> => {
+	switch (action.type) {
+		case 'sort':
+			const newState = [...state].sort ( ( a, b ) => {
+				if (a.name > b.name) {
+					return 1
+				} else if (a.name < b.name) {
+					return -1
+				} else {
+					return 0
+				}
+			} )
+
+			return (action.payload === 'up') ? newState : newState.reverse ()
+
+		case 'check': {
+			const newState = [...state].filter ( s => s.age > 18 )
+			return newState
+		}
+		default:
+			return state
+	}
 }
+
