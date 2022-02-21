ff6b784a19664d4114834609c86e0cb781760a68
makcsholonik <makcsholonik@gmail.com>
2022-02-22 02:27:47 +0300

    HW11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,39 +1,39 @@
-import React, {useState} from 'react'
-import SuperRange from './common/c7-SuperRange/SuperRange'
-import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import React, { useState } from 'react';
+import SuperRange from './common/c7-SuperRange/SuperRange';
 
-function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+function HW11 () {
+	const [value1, setValue1] = useState ( 0 );
+	// const [value2, setValue2] = useState ( 100 );
 
-    return (
-        <div>
-            <hr/>
-            homeworks 11
+	const setValueSuperRange = ( newValue : number ) => setValue1 ( newValue );
 
-            {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
-            </div>
+	return (
+		<div>
+			<hr/>
+			homeworks 11
+			<div>
+				<span>{ value1 }</span>
+				<SuperRange
+					value1={ value1 }
+					setValueSuperRange={ setValueSuperRange }
+				/>
+			</div>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
-                />
-                <span>{value2}</span>
-            </div>
+			{/*<div>
+				<span>{ value1 }</span>
+				<SuperDoubleRange
+					// сделать так чтоб value1 и value2 изменялось
+				/>
+				<span>{ value2 }</span>
+			</div>*/}
 
-            <hr/>
-            {/*для личного творчества, могу проверить*/}
-            {/*<AlternativeSuperRange/>*/}
-            {/*<AlternativeSuperDoubleRange/>*/}
-            <hr/>
-        </div>
-    )
+			<hr/>
+			{/*для личного творчества, могу проверить*/ }
+			{/*<AlternativeSuperRange/>*/ }
+			{/*<AlternativeSuperDoubleRange/>*/ }
+			<hr/>
+		</div>
+	);
 }
 
-export default HW11
+export default HW11;
